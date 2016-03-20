7d762e595764c682a165499c99cebc430c7d5a33
SangHyeon <sktkddusrj@naver.com>
2016-03-20 22:36:41 +0900

    test

--- 150318/1937.cpp
+++ 150318/1937.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<deque>
 using namespace std;
 
 int table[501][501] = {0,};
@@ -8,22 +9,55 @@ int n;
 int MAX=0;
 
 void dfs(int x, int y);
+struct Point {
+    int x, y, level;
+};
 
 int main() {
+    deque<Point> q;
+    int dx[] = {1, 0, -1, 0};
+    int dy[] = {0, 1, 0, -1};
     cin>>n;
 
     for(int i=0; i<n; i++)
         for(int j=0; j<n; j++)
             cin>>table[i][j];
 
+    int visited_level = 0;
     for(int i=0; i<n; i++) {
         for(int j=0; j<n; j++) {
             visited[i][j] = 1;
-            dfs(i, j);/*
+            Point start;
+            start.x = i;
+            start.y = j;
+            start.level = 1;
+            q.push_back(start);
+            while(!q.empty()) {
+                Point cur = q.front();
+                q.pop_front();
+                for(int i=0; i<3; i++) {
+                    int x = cur.x + dx[i];
+                    int y = cur.y + dy[i];
+
+                    if(x >= 0 && x < n && y >= 0 && y < n && visited_level == visited[x][y] \
+                            && table[i][j] < table[x][y] && max_v[x][y] < cur.level+1) {
+                        Point t;
+                        t.x = x;
+                        t.y = y;
+                        t.level = cur.level+1;
+                        visited[x][y]++;
+                        q.push_back(t);
+                        if(MAX < t.level)
+                            MAX = t.level;
+                    }
+                }
+            }
+            visited_level++;
+            /*dfs(i, j);
             for(int k=0;k<n;k++)
                 for(int l=0;l<n;l++)
                     visited[k][l] = 0;
-                    */
+            */
         }
     }
 
